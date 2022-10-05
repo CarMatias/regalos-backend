aeea261218d99be6892428d630e603ec4a2b7352
CarMatias <matiasbcar_31@hotmail.com>
2022-10-05 09:22:39 -0300

    Modifico carga regalos en BD - agrego GET de recomendaciones

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -14,7 +14,12 @@ router.use(bodyParser.urlencoded({extended: true }))
 router.post('/uploadgift',cors(corsOptions),async (req, res)=>{
     const name = req.body.name
     const image = req.body.image
-    const newGift = await UploadGiftService.uploadGift(name,image)
-    res.send(newGift)
+    const tag = req.body.tag
+    if(name != null && image != null && tag != null){
+      const newGift = await UploadGiftService.uploadGift(name,image,tag)
+      res.send(newGift)
+    }else{
+      res.send("Debe enviar un name, image y tag como parametros")
+    }
   })
   export default router
